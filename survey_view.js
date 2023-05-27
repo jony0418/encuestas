eaca2f823bc7418abd4e65e4aca96418a34003bd
Jonathan Becerra <78985884+jony0418@users.noreply.github.com>
2023-05-27 11:50:20 -0600

    Update survey_view.js

--- survey_view.js
+++ survey_view.js
@@ -1,78 +1,4 @@
-function displaySurvey(survey) {
-    let form = document.getElementById('surveyAnswersForm');
-  
-    survey.forEach(function(questionObj, index) {
-      let questionElement = document.createElement('p');
-      questionElement.textContent = questionObj.question;
-      form.appendChild(questionElement);
-  
-      let answerElement;
-  
-      switch(questionObj.type) {
-        case 'open':
-          answerElement = document.createElement('textarea');
-          answerElement.name = 'answer' + index;
-          form.appendChild(answerElement);
-          break;
-        
-        case 'truefalse':
-          ['verdadero', 'falso'].forEach(function(option) {
-            let label = document.createElement('label');
-            answerElement = document.createElement('input');
-            answerElement.type = 'radio';
-            answerElement.name = 'answer' + index;
-            answerElement.value = option;
-            label.appendChild(answerElement);
-            label.appendChild(document.createTextNode(option));
-            form.appendChild(label);
-          });
-          break;
-  
-        case 'satisfaction':
-          ['Muy Mal', 'Mal', 'Regular', 'Bien', 'Muy Bien'].forEach(function(option) {
-            let label = document.createElement('label');
-            label.className = 'satisfactionOption';
-            answerElement = document.createElement('input');
-            answerElement.type = 'radio';
-            answerElement.name = 'answer' + index;
-            answerElement.value = option;
-            label.appendChild(answerElement);
-            label.appendChild(document.createTextNode(option));
-            form.appendChild(label);
-          });
-          break;
-      }
-    });
-  }
-  
-  window.onload = function() {
-    let surveyNameSelector = document.getElementById('surveyName');
-    let surveys = JSON.parse(localStorage.getItem('surveys')) || {};
-  
-    for (let surveyName in surveys) {
-      let option = document.createElement('option');
-      option.value = surveyName;
-      option.textContent = surveyName;
-      surveyNameSelector.appendChild(option);
-    }
-  };
-  
-  document.getElementById('surveyName').addEventListener('change', function() {
-    let form = document.getElementById('surveyAnswersForm');
-    // Borra todas las preguntas actuales
-    while (form.firstChild) {
-      form.removeChild(form.firstChild);
-    }
-    
-    let surveyName = this.value;
-    let surveys = JSON.parse(localStorage.getItem('surveys')) || {};
-    let survey = surveys[surveyName];
-  
-    // Muestra las preguntas de la encuesta seleccionada
-    displaySurvey(survey);
-  });
-  
- document.getElementById('submitAnswers').addEventListener('click', function() {
+document.getElementById('submitAnswers').addEventListener('click', function() {
   let name = document.getElementById('name').value;
   let nomina = document.getElementById('nomina').value;
   let surveyName = document.getElementById('surveyName').value;
@@ -99,6 +25,12 @@ function displaySurvey(survey) {
       return;
     }
   }
+
+  // Envía las respuestas al archivo PHP a través de una solicitud AJAX
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "saveAnswers.php", true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.send("nomina=" + nomina + "&surveyName=" + surveyName + "&answers=" + JSON.stringify(answers));
 
   let responses = JSON.parse(localStorage.getItem('responses')) || {};
   let id = 'nomina-' + nomina + '-survey-' + surveyName;
